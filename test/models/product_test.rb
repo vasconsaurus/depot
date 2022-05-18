76677c3bc83fc7a0e00fb3beefa8b462ffe36ca4
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-05-18 10:46:16 -0300

    edit products controller test, end of iteration B2 validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -60,6 +60,7 @@ class ProductTest < ActiveSupport::TestCase
                           image_url: "fred.gif")
 
     assert product.invalid?
-    assert_equal ["has already been taken"], product.errors[:title]
+    # assert_equal ["has already been taken"], product.errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 end
