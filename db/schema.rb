e4e66834ca78c3cd73ce73d05e1a773074dc59ba
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-05-20 13:41:00 -0300

    d2: added current cart module to concerns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_174701) do
+ActiveRecord::Schema.define(version: 2022_05_20_144015) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
