fd43e4097ccd2f17b5526ba206d2093f45941d71
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-23 15:26:27 -0300

    email is working
    I had forgotten to update the from email in
    order_mailer and application_mailer (facepalm)

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,5 @@
 class OrderMailer < ApplicationMailer
-  default from: 'Manu <depot@example.org>'
+  default from: ENV['SECRET_EMAIL']
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
