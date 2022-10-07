c3f4f852392e0d83bc8287142b320cdb71b3153d
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-10-07 16:13:11 -0300

    wip: sends an email when an error occurrs in the app
    It sends an email, but we get a blank page of death.
    And I'm not sending the error logging info, so it
    just let's me know an error has happened. Not great.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :email_error
+
+  def email_error
+    logger.error 'An error occourred'
+    ApplicationMailer.error_notification_email.deliver_later
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV['SECRET_EMAIL']
   layout 'mailer'
+
+  def error_notification_email
+    mail to: ENV['SECRET_EMAIL'], subject: 'An error has occourred'
+  end
 end
